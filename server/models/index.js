4d46a43a791ff16f9da7526bb9531e74f2420a81
Hoyong Lee <igotoweb@gmail.com>
2021-02-24 13:49:18 +0900

    initial commit

---
+++ server/models/index.js
@@ -0,0 +1,20 @@
+const db = require('../db');
+
+module.exports = {
+  orders: {
+    get: (userId, callback) => {
+      // TODO: 해당 유저가 작성한 모든 주문을 가져오는 함수를 작성하세요
+      callback(/* err, result */);
+    },
+    post: (userId, orders, totalPrice, callback) => {
+      // TODO: 해당 유저의 주문 요청을 데이터베이스에 생성하는 함수를 작성하세요
+      callback(/* err, result */);
+    }
+  },
+  items: {
+    get: (callback) => {
+      // TODO: Cmarket의 모든 상품을 가져오는 함수를 작성하세요
+      callback(/* err, result */);
+    }
+  }
+};
